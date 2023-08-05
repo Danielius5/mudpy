c42f08765532966dc8da080745d9c0bb30a61614
Alyce <Alyceosbourne@outlook.com>
2023-08-05 01:38:02 +0100

    Edited states to be more.... stateful

--- descriptors/state.py
+++ descriptors/state.py
@@ -4,7 +4,6 @@ class State:
         self.default = default
         self.default_factory = default_factory
         self.type = _type or type(default) if default is not None else None
-        
 
     def __set_name__(self, owner, name):
         self.name = name
@@ -25,21 +24,52 @@ class State:
     def __delete__(self, instance):
         del instance.__dict__[self.name]
 
-class IntState(State):
-    def __init__(self, min, max, *args, **kwargs):
-        super().__init__(lambda x: min <= x <= max, *args, _type = int, **kwargs)
-        
-        
-class FloatState(State):
-    def __init__(self, min, max, *args, **kwargs):
-        super().__init__(lambda x: min <= x <= max, *args, _type = float, **kwargs)
-        
-        
-class StringState(State):
-    def __init__(self, min_length, max_length, *args, **kwargs):
-        super().__init__(lambda x: min_length <= len(x) <= max_length, *args, _type = str, **kwargs)
-        
 
+# noinspection PyArgumentList
+class NumberState(State):
+    def __init__(self, minimum = None, maximum = None, *args, **kwargs):
+        constraint = None
+        match minimum, maximum:
+            case None, None:
+                constraint = lambda x: True
+            case None, int():
+                constraint = lambda x: x <= maximum
+            case int(), None:
+                constraint = lambda x: minimum <= x
+            case int(), int():
+                constraint = lambda x: minimum <= x <= maximum
+        super().__init__(constraint, *args, **kwargs)
+
+
+# noinspection PyArgumentList
+class IntState(NumberState):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, _type = int, **kwargs)
+
+
+# noinspection PyArgumentList
+class FloatState(NumberState):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, _type = float, **kwargs)
+
+
+# noinspection PyArgumentList
+class StringState(State):
+    def __init__(self, min_length=None, max_length=None, *args, **kwargs):
+        constraint = None
+        match min_length, max_length:
+            case None, None:
+                constraint = lambda x: True
+            case None, int():
+                constraint = lambda x: len(x) <= max_length
+            case int(), None:
+                constraint = lambda x: min_length <= len(x)
+            case int(), int():
+                constraint = lambda x: min_length <= len(x) <= max_length
+        super().__init__(constraint, *args, **kwargs)
+
+
+# noinspection PyArgumentList
 class BoolState(State):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, _type = bool, **kwargs)
